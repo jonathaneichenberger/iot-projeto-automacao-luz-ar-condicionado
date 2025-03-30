779662bb737c9e96b40a8d2d04c1948559acb968
Jonathan Eichenberger <djohnni@hotmail.com>
2025-03-30 07:09:46 -0300

    Implementando as Bibliotecas que serão usadas no projeto

---
+++ lib/funcoes.h
@@ -0,0 +1,12 @@
+#ifndef FUNCOES_H
+#define FUNCOES_H
+
+#include <Arduino.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
+#include <LiquidCrystal.h>
+#include <Adafruit_NeoPixel.h>
+#include <ThingSpeak.h>
+
+#endif // FUNCOES_H
