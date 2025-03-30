01196b5d8562737339c9737217628ea57978d6be
Jonathan Eichenberger <djohnni@hotmail.com>
2025-03-30 07:19:22 -0300

    Adicionando a definição dos Pinos que serão usados e comentários de onde serão usados

--- lib/funcoes.h
+++ lib/funcoes.h
@@ -1,12 +1,23 @@
 #ifndef FUNCOES_H
 #define FUNCOES_H
 
-#include <Arduino.h>
-#include <WiFi.h>
-#include <PubSubClient.h>
-#include <ArduinoJson.h>
-#include <LiquidCrystal.h>
-#include <Adafruit_NeoPixel.h>
-#include <ThingSpeak.h>
+#include <Arduino.h>          // Biblioteca principal para programação em placas Arduino
+#include <WiFi.h>             // Permite conexão Wi-Fi para ESP32 e ESP8266
+#include <PubSubClient.h>     // Implementa o protocolo MQTT para comunicação IoT
+#include <ArduinoJson.h>      // Facilita a manipulação de dados em formato JSON
+#include <LiquidCrystal.h>    // Controla displays LCD baseados no controlador HD44780
+#include <Adafruit_NeoPixel.h>// Controla LEDs RGB endereçáveis da Adafruit (NeoPixels)
+#include <ThingSpeak.h>       // Facilita a comunicação com a plataforma IoT ThingSpeak
+
+// Definição dos pinos
+#define LAMP_PIXEL_PIN 6  // Pino do NeoPixel das lâmpadas
+#define AIR_PIXEL_PIN 32  // Pino do NeoPixel do ar-condicionado
+#define PIXEL_COUNT 16    // Quantidade de LEDs nos NeoPixels
+#define LCD_RS 19         // Pino RS do LCD
+#define LCD_E 18          // Pino E do LCD
+#define LCD_D4 5          // Pino D4 do LCD
+#define LCD_D5 21         // Pino D5 do LCD
+#define LCD_D6 2          // Pino D6 do LCD
+#define LCD_D7 15         // Pino D7 do LCD
 
 #endif // FUNCOES_H
